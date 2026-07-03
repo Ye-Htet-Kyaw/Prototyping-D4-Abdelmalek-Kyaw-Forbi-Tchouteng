bbdc3707857f3aea6dcbb8323fba433ced808b6f
gabrieljake <gabrieltchakunte@gmail.com>
2026-07-03 20:14:21 +0200

    Add setup function for pin configuration

---
+++ Firmware and Simulation/Earlier Versions/Alternative/Setup.cpp
@@ -0,0 +1,16 @@
+void setup() {
+  pinMode(enA, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(enB, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+
+  pinMode(irLeft, INPUT);
+  pinMode(irRight, INPUT);
+
+  pinMode(trigLeft, OUTPUT);
+  pinMode(echoLeft, INPUT);
+  pinMode(trigRight, OUTPUT);
+  pinMode(echoRight, INPUT);
+}
